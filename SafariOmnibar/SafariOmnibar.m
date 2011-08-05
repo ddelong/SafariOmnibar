be57f477af6f2164e22ce2b20a7db3217d7bbf8c
Derik DeLong <github@s.derik.us>
2011-08-04 18:24:24 -0700

    Correctly resolve local host names without a TLD
    This allows one to type things as simple as "localhost" and have them
    resolve correctly, rather than force the user to type out the protocol.

--- SafariOmnibar/SafariOmnibar.m
+++ SafariOmnibar/SafariOmnibar.m
@@ -43,9 +43,10 @@ NSString * const kOmnibarSearchProviders = @"SafariOmnibar_SearchProviders";
                 url = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", location]];
             }
         }
-        if (!url || (![url.host isEqualToString:@"about"] && ![NSHost hostWithName:url.host].address))
+        if ((!url || (![url.host isEqualToString:@"about"] && ![NSHost hostWithName:url.host].address))
+            && ![NSHost hostWithName:location].address)
         {
-            // When location can't be parsed as URL or URL's host part can't be resolved, perform a search using the default search provider
+            // When location can't be parsed as URL or URL's host part can't be resolved, perform a search using the default search provider, and the string itself isn't a hostname
             searchURLTemplate = [[plugin defaultSearchProvider] objectForKey:@"SearchURLTemplate"];
         }
     }
